3ab2d61d6b34fa87082258af3c533d49ef90e075
cryptomehedi <classicalmehedi@gmail.com>
2022-02-08 17:57:49 +0600

    js Fixed

--- JS/events.js
+++ JS/events.js
@@ -96,22 +96,13 @@ const ClickText = document.getElementById('textP');
     });
 // delegate event 
 
-    // const items= document.getElementsByClassName('item');
-        // for(const item of items){
-        //     item.addEventListener('click',function(event){
-        //         // document.getElementById('item-container').removeChild(item);
-        //         event.target.parentNode.removeChild(event.target)
+document.getElementById('add-item').addEventListener('click', function(){
+    const li = document.createElement('li');
+    li.innerText= 'New Item';
+    const parent = document.getElementById('item-container');
+    parent.appendChild(li);
+});
 
-        //     });
-        // }
-
-        document.getElementById('add-item').addEventListener('click', function(){
-            const li = document.createElement('li');
-            li.innerText= 'New Item';
-            const parent = document.getElementById('item-container');
-            parent.appendChild(li);
-        });
-
-        document.getElementById('item-container').addEventListener('click', function(event){
-            event.target.parentNode.removeChild(event.target)
-        });
+document.getElementById('item-container').addEventListener('click', function(event){
+    event.target.parentNode.removeChild(event.target)
+});
